e158cdd66ed3202a30a85cc694a118810ff5d9dd
Josh <jseibel@wisc.edu>
2021-04-02 13:12:58 -0500

    Update google-script.js

--- js/google-script.js
+++ js/google-script.js
@@ -75,7 +75,7 @@ function initClient() {
     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
   }).then(function() {
     gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
   });
 }
 
